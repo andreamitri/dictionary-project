51b89b2ff500b473015e2dc482405945b5a2145d
andreamitri <ladanandrea@gmail.com>
2024-11-06 22:22:49 +0100

    Added dictionary

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">hello</header>
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Andrea</small>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
